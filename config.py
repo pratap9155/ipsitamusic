4a5b7cd749e25339c979d0e5a78b67cd3dc735bf
Venom Pratap <venompratap22@gmail.com>
2025-08-22 14:33:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,6 +22,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/pratap9155/ipsitamusic",)
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
+
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY",) # youtube song api ke
 PRIVACY_LINK = getenv("PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-YukkiMusic-08-30")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VENOMPRATAP")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/KRITI_VENOMPRATAPCHAT")
